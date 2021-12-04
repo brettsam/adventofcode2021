16980941e867d371d63baf38a989724c74d7554e
Brett Samblanet <brettsam@gmail.com>
2021-12-04 13:07:03 -0500

    day1, part2

--- Day1/main.go
+++ Day1/main.go
@@ -8,6 +8,12 @@ import (
 
 func main() {
 	measurements := input.GetMeasurements()
+
+	fmt.Println(partOne(measurements))
+	fmt.Println(partTwo(measurements))
+}
+
+func partOne(measurements []int) int {
 	count := 0
 	for i := 1; i < len(measurements); i++ {
 		last := measurements[i-1]
@@ -16,5 +22,19 @@ func main() {
 			count++
 		}
 	}
-	fmt.Println(count)
+	return count
+}
+
+func partTwo(measurements []int) int {
+	count := 0
+	for i := 3; i < len(measurements); i++ {
+		one := measurements[i-3]
+		two := measurements[i-2]
+		three := measurements[i-1]
+		four := measurements[i]
+		if four+three+two > three+two+one {
+			count++
+		}
+	}
+	return count
 }
